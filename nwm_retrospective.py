42abbe65fdd4fab63643b57805ead83fefb74ff1
Kurt Wolfe <kurt.wolfe@gmail.com>
2025-04-02 13:52:44 -0400

    Remove unneeded files for desktop python executable

---
+++ nwm_retrospective.py
@@ -0,0 +1,80 @@
+import sys
+import logging
+import logging.handlers
+from logging.handlers import TimedRotatingFileHandler
+import json
+from datetime import datetime
+from nwm_utils import get_data
+
+LOGFILE = "nwm_retrospective.log"
+logger = logging.getLogger("")
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        TimedRotatingFileHandler(LOGFILE, when="midnight", interval=1),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+#National Water Model (NWM) CONUS Retrospective Dataset
+# https://registry.opendata.aws/nwm-archive/
+# Dataset - February 1979 through January 2023
+start_data = datetime.strptime("1979-02-01", "%Y-%m-%d")  # Start of the NWM retrospective dataset
+end_data = datetime.strptime("2023-01-31", "%Y-%m-%d")  # End of the NWM retrospective dataset
+
+    
+#Read parameters from a file and return the data a JSON object
+def read_params(file_path: str):
+    """
+    Read parameters from a file and return the data.
+    """
+    params = ""
+    try:
+        with open(file_path, 'r') as f:
+            params = f.read()
+            logger.info(f"Successfully read from {file_path}")
+            return params
+    except FileNotFoundError:
+        logger.error(f"File not found: {file_path}")
+        return None
+    except PermissionError:
+        logger.error(f"Error: You do not have permission to access this file: {file_path}")
+        return None
+    except IOError as e:
+        logger.error(f"An I/O error occurred: {file_path}")
+        return None
+    except Exception as e:        
+        logger.error(f"Failed to read from {file_path}: {e}")
+        return None
+        
+    return params
+
+
+if __name__ == "__main__":
+    
+    arg1 = "" 
+    if len(sys.argv) < 2:
+        logger.error("No file path provided. Please provide a file path as a command line argument.")
+        sys.exit(1)
+    for i, arg in enumerate(sys.argv[1:]):
+        logger.info(f"Argument {i+1}: {arg}")
+        arg1 = arg
+
+    params = read_params(arg1)
+    if params is None:
+        logger.error("Failed to read parameters from the file.")
+        sys.exit(1)
+
+    param_data = json.loads(params)  # Assuming the file contains JSON data
+    start_date = param_data['date_range']['start']
+    end_date = param_data['date_range']['end']
+    comids = param_data['comids']
+    if len(comids) == 0:
+        logger.error("No COMIDs provided in the input file.")
+        sys.exit(1)
+    file_prefix = param_data['file_prefix']
+
+    file_path = get_data(start_date, end_date, comids, file_prefix)
+
+    logger.info(f"Successfully wrote results to: {file_path}")
--- nwm_utils.py
+++ nwm_utils.py
@@ -1,9 +1,11 @@
 import xarray as xr
 import fsspec
 from fsspec import FSMap
-import pyproj
-import geopandas as gpd
 import zarr
+import logging
+import s3fs
+
+logger = logging.getLogger("")
 
 def get_conus_bucket_url(variable_code):
     """
@@ -29,9 +31,11 @@ def load_dataset(conus_bucket_url):
     Returns:
     xr.Dataset: The loaded dataset as a xarray dataset
     """
+    logger.info(f"Loading dataset from {conus_bucket_url}")
     fs = fsspec.filesystem('s3', anon=True, asynchronous=True)  # Use anonymous access to S3
     store = zarr.storage.FsspecStore(fs, path=conus_bucket_url)
     ds = xr.open_zarr(store, consolidated=True)  # Load the dataset using xarray
+    logger.info(f"Dataset loaded successfully from {conus_bucket_url}")
     return ds
     #ds = xr.open_zarr(
     #    fsspec.get_mapper(
@@ -42,41 +46,7 @@ def load_dataset(conus_bucket_url):
     #)
     #return ds
 
-def reproject_coordinates(ds, lon, lat, input_crs='EPSG:4326'):
-    """
-    This function reprojects the given lon and lat coordinates to the dataset's CRS.
-    
-    Parameters:
-    ds (xr.Dataset): The dataset containing the CRS information.
-    lon (float): The longitude to reproject.
-    lat (float): The latitude to reproject.
-    input_crs (str): The CRS of the input coordinates.
 
-    Returns:
-    tuple: The reprojected coordinates (x, y).
-    """
-    output_crs = pyproj.CRS(ds.crs.esri_pe_string)  # CRS of AORC dataset (lcc)
-    transformer = pyproj.Transformer.from_crs(input_crs, output_crs, always_xy=True)
-    x, y = transformer.transform(lon, lat)
-    return x, y
-
-def get_fid(usgs_gageid):
-    """
-    Get the feature id for a given USGS gage id.
-    
-    Paramters:
-    usgs_gageid (str): USGS gage ID
-    
-    Returns:
-    str: Feature id
-    """
-    # Read the linked sites GeoPackage
-    linked_sites = gpd.read_file("./linked_sites.gpkg")
-    
-    # Retrieve the COMID for the given USGS gage ID
-    fid_value = linked_sites.loc[linked_sites['site_id'] == usgs_gageid, 'COMID'].iloc[0]
-    
-    return fid_value
 
 def get_aggregation_code(aggr_name):
     """
@@ -99,3 +69,53 @@ def get_aggregation_code(aggr_name):
         raise Exception(f"{aggr_name} is not a valid aggregation name")
     
     return agg_options[aggr_name]
+
+def get_data(start_date:str, end_date:str, comids:list, file_name:str):
+    
+    # Start date in Year-Month-Day format, the earliest start date can be '1979-02-01'    
+    # User-defined NWM output dataset; see above for a list of valid dataset names.
+    #nwm_out_ds='Land Surface Model Output'
+    nwm_out_ds = 'Streamflow output at all channel reaches/cells'
+
+    # User-defined variable name - this variable is part of the National Water Model (NWM) output datasets
+    # For a full list of NWM output variables, refer to the following website: https://ral.ucar.edu/sites/default/files/public/WRFHydroV5_OutputVariableMatrix_V5.pdf
+    #variable_name = 'ACCET'
+    variable_name = 'streamflow'
+
+    # User-defined aggregation interval - valid values are 'hour', 'day', 'month', 'year'
+    agg_interval='hour'
+
+    # Set dictionary to map output name onto associated code used in the AWS bucket storage
+    nwm_outputs = {
+        'Land Surface Model Output': 'ldasout', 
+        'Terrain Routing Output': 'rtout', 
+        'Land Surface Diagnostic Output': 'lsmout', 
+        'Streamflow output at all channel reaches/cells': 'chrtout'
+    }
+
+    # Get the name of the NWM output dataset from the dictionary (nwm_outputs)
+    variable_code = nwm_outputs[nwm_out_ds]
+
+    # Get the variable code for the aggregation interval
+    agg_code = get_aggregation_code(agg_interval)
+
+    # Get the S3 bucket URL for the data path
+    url = get_conus_bucket_url(variable_code)
+    ds = load_dataset(url)        
+    
+    # Load the dataset    
+    com_ids = comids
+    length = len(com_ids)
+    
+    ds_subset = ds[variable_name].sel(feature_id=ds.feature_id.isin(com_ids)).loc[dict(time=slice(start_date,end_date))]
+    #ds_subset_df = ds_subset.mean(['x', 'y']).to_dataframe()
+    # Specify the file path where you want to save the CSV file
+    file_path = f"{file_name}_{start_date}_{end_date}.csv"
+
+    # Save the DataFrame to a CSV file
+    logger.info(f"Saving data to dataframe")
+    df = ds_subset.to_dataframe()
+    logger.info(f"Saving data to {file_path}")
+    df.to_csv(file_path, index=True)
+            
+    return file_path
